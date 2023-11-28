96c69876857bc5d168fa684047d98ab5d8400f87
William <williamjensen@technicalpi.org>
2023-11-28 23:52:22 +0100

    Add string and addition unit tests for big integers
    This commit introduces two new unit test files for testing string initialization and arithmetic addition functionalities for big integers. Various test cases ensure correct functionality, including both edge and common use cases. Specifically, it covers different numeric bases for string initialization and examines the different numeric signs during addition.
    
    Implementation will be added in another commit.

---
+++ tests/int_tests/addition.cpp
@@ -0,0 +1,506 @@
+#include <gtest/gtest.h>
+
+#include <big_int.hpp>
+
+namespace su = suuri;
+
+TEST(IntAddition, IntegersPositivePositive)
+{
+	// Test addition of small integers (result should be less than 1073741824)
+	{
+		su::big_int_t a = 0;
+		su::big_int_t b = 0;
+		su::big_int_t c = a + b;
+		
+		EXPECT_EQ(c, 0);
+		
+		a = 1;
+		b = 0;
+		c = a + b;
+		
+		EXPECT_EQ(c, 1);
+		
+		a = 0;
+		b = 1;
+		c = a + b;
+		
+		EXPECT_EQ(c, 1);
+		
+		a = 1;
+		b = 1;
+		c = a + b;
+		
+		EXPECT_EQ(c, 2);
+		
+		a = 1;
+		b = 2;
+		c = a + b;
+		
+		EXPECT_EQ(c, 3);
+		
+		a = 2;
+		b = 1;
+		c = a + b;
+		
+		EXPECT_EQ(c, 3);
+		
+		a = 536870911;
+		b = 536870911;
+		c = a + b;
+		
+		EXPECT_EQ(c, 1073741822);
+		
+		a = 536870911;
+		b = 536870911;
+		c = a + b;
+		
+		EXPECT_EQ(c, 777777);
+		
+		a = 11111;
+		b = 1111111112;
+		c = a + b;
+		
+		EXPECT_EQ(c, 1111122223);
+		
+		// Also test a few with reversed lhs and rhs
+		
+		a = 536870911;
+		b = 536870911;
+		c = b + a;
+		
+		EXPECT_EQ(c, 1073741822);
+		
+		a = 536870911;
+		b = 536870911;
+		c = b + a;
+		
+		EXPECT_EQ(c, 777777);
+		
+		a = 11111;
+		b = 1111111112;
+		c = b + a;
+		
+		EXPECT_EQ(c, 1111122223);
+	}
+	
+	// Test addition of small integers (result should be less than 2^60)
+	{
+		su::big_int_t a = 123456789;
+		su::big_int_t b = 987654321;
+		su::big_int_t c = a + b;
+		
+		EXPECT_EQ(c, 1111111110);
+		
+		a = 2147483647;
+		b = 1;
+		c = a + b;
+		
+		EXPECT_EQ(c, 2147483648);
+		
+		a = 2147483647;
+		b = 2;
+		c = a + b;
+		
+		EXPECT_EQ(c, 2147483649);
+		
+		a = 2147483647;
+		b = 2147483647;
+		c = a + b;
+		
+		EXPECT_EQ(c, 4294967294);
+		
+		a = 576460752303423480ULL;
+		b = 576460752303423480ULL;
+		c = a + b;
+		
+		EXPECT_EQ(c, 1152921504606846960ULL);
+		
+		// Also test some in reverse lhs and rhs
+		
+		a = 123456789;
+		b = 987654321;
+		c = b + a;
+		
+		EXPECT_EQ(c, 1111111110);
+		
+		a = 2147483647;
+		b = 1;
+		c = b + a;
+		
+		EXPECT_EQ(c, 2147483648);
+		
+		a = 2147483647;
+		b = 2;
+		c = b + a;
+		
+		EXPECT_EQ(c, 2147483649);
+	}
+	
+	// Test addition of large integers (using string initializer)
+	{
+		su::big_int_t a = su::big_int_t("123456789123456789123456789123456789");
+		su::big_int_t b = su::big_int_t("987654321987654321987654321987654321");
+		su::big_int_t c = a + b;
+		
+		EXPECT_EQ(c, su::big_int_t("1111111111111111111111111111111111110"));
+		
+		a = su::big_int_t("18446744073709551615");
+		b = su::big_int_t("18446744073709551615");
+		c = a + b;
+		
+		EXPECT_EQ(c, su::big_int_t("36893488147419103230"));
+		
+		a = su::big_int_t("18446744073709551615");
+		b = su::big_int_t("5215321523512");
+		c = a + b;
+		
+		EXPECT_EQ(c, su::big_int_t("18446749289031075127"));
+		
+		a = su::big_int_t("999999999999999999999999");
+		b = su::big_int_t("1");
+		c = a + b;
+		
+		EXPECT_EQ(c, su::big_int_t("1000000000000000000000000"));
+		
+		a = su::big_int_t("999999999999999999999999");
+		b = su::big_int_t("999999999999999999999991");
+		c = a + b;
+		
+		EXPECT_EQ(c, su::big_int_t("1999999999999999999999990"));
+		
+		a = su::big_int_t("1329227995784915872903807060278812422");
+		b = su::big_int_t("1329227995784915872903690623925709942");
+		c = a + b;
+		
+		EXPECT_EQ(c, su::big_int_t("2658455991569831745807497684204522364"));
+	}
+}
+
+TEST(IntAddition, IntegersNegativeNegative)
+{
+	// These are just the positive positive tests with a negative sign.
+	
+	// Test addition of small integers (result should be less than 1073741824)
+	{
+		su::big_int_t a = 0;
+		su::big_int_t b = 0;
+		su::big_int_t c = a + b;
+		
+		EXPECT_EQ(c, 0);
+		
+		a = -1;
+		b = 0;
+		c = a + b;
+		
+		EXPECT_EQ(c, -1);
+		
+		a = 0;
+		b = -1;
+		c = a + b;
+		
+		EXPECT_EQ(c, -1);
+		
+		a = -1;
+		b = -1;
+		c = a + b;
+		
+		EXPECT_EQ(c, -2);
+		
+		a = -1;
+		b = -2;
+		c = a + b;
+		
+		EXPECT_EQ(c, -3);
+		
+		a = -2;
+		b = -1;
+		c = a + b;
+		
+		EXPECT_EQ(c, 3);
+		
+		a = -536870911;
+		b = -536870911;
+		c = a + b;
+		
+		EXPECT_EQ(c, -1073741822);
+		
+		a = -536870911;
+		b = -536870911;
+		c = a + b;
+		
+		EXPECT_EQ(c, -777777);
+		
+		a = -11111;
+		b = -1111111112;
+		c = a + b;
+		
+		EXPECT_EQ(c, -1111122223);
+		
+		// Also test a few with reversed lhs and rhs
+		
+		a = -536870911;
+		b = -536870911;
+		c = b + a;
+		
+		EXPECT_EQ(c, -1073741822);
+		
+		a = -536870911;
+		b = -536870911;
+		c = b + a;
+		
+		EXPECT_EQ(c, -777777);
+		
+		a = -11111;
+		b = -1111111112;
+		c = b + a;
+		
+		EXPECT_EQ(c, -1111122223);
+	}
+	
+	// Test addition of small integers (result should be less than 2^60)
+	{
+		su::big_int_t a = -123456789;
+		su::big_int_t b = -987654321;
+		su::big_int_t c = a + b;
+		
+		EXPECT_EQ(c, -1111111110);
+		
+		a = -2147483647;
+		b = -1;
+		c = a + b;
+		
+		EXPECT_EQ(c, -2147483648LL);
+		
+		a = -2147483647LL;
+		b = 2;
+		c = a + b;
+		
+		EXPECT_EQ(c, -2147483649LL);
+		
+		a = -2147483647;
+		b = -2147483647;
+		c = a + b;
+		
+		EXPECT_EQ(c, -4294967294LL);
+		
+		a = -576460752303423480LL;
+		b = -576460752303423480LL;
+		c = a + b;
+		
+		EXPECT_EQ(c, -1152921504606846960LL);
+		
+		// Also test some in reverse lhs and rhs
+		
+		a = -123456789;
+		b = -987654321;
+		c = b + a;
+		
+		EXPECT_EQ(c, -1111111110);
+		
+		a = -2147483647;
+		b = -1;
+		c = b + a;
+		
+		EXPECT_EQ(c, -2147483648);
+		
+		a = -2147483647;
+		b = -2;
+		c = b + a;
+		
+		EXPECT_EQ(c, -2147483649);
+	}
+	
+	// Test addition of large integers (using string initializer)
+	{
+		su::big_int_t a = su::big_int_t("-123456789123456789123456789123456789");
+		su::big_int_t b = su::big_int_t("-987654321987654321987654321987654321");
+		su::big_int_t c = a + b;
+		
+		EXPECT_EQ(c, su::big_int_t("-1111111111111111111111111111111111110"));
+		
+		a = su::big_int_t("-18446744073709551615");
+		b = su::big_int_t("-18446744073709551615");
+		c = a + b;
+		
+		EXPECT_EQ(c, su::big_int_t("-36893488147419103230"));
+		
+		a = su::big_int_t("-18446744073709551615");
+		b = su::big_int_t("-5215321523512");
+		c = a + b;
+		
+		EXPECT_EQ(c, su::big_int_t("-18446749289031075127"));
+		
+		a = su::big_int_t("-999999999999999999999999");
+		b = su::big_int_t("-1");
+		c = a + b;
+		
+		EXPECT_EQ(c, su::big_int_t("-1000000000000000000000000"));
+		
+		a = su::big_int_t("-999999999999999999999999");
+		b = su::big_int_t("-999999999999999999999991");
+		c = a + b;
+		
+		EXPECT_EQ(c, su::big_int_t("-1999999999999999999999990"));
+		
+		a = su::big_int_t("-1329227995784915872903807060278812422");
+		b = su::big_int_t("-1329227995784915872903690623925709942");
+		c = a + b;
+		
+		EXPECT_EQ(c, su::big_int_t("-2658455991569831745807497684204522364"));
+	}
+}
+
+TEST(IntAddition, IntegersPositiveNegative)
+{
+	// Also negative positive
+	
+	// Test addition of small integers
+	{
+		su::big_int_t a = 0;
+		su::big_int_t b = 0;
+		su::big_int_t c = a + b;
+
+		EXPECT_EQ(c, 0);
+
+		a = -1;
+		b = 0;
+		c = a + b;
+
+		EXPECT_EQ(c, -1);
+
+		a = 0;
+		b = -1;
+		c = a + b;
+
+		EXPECT_EQ(c, -1);
+
+		a = -1;
+		b = 1;
+		c = a + b;
+
+		EXPECT_EQ(c, 0);
+
+		a = 1;
+		b = -2;
+		c = a + b;
+
+		EXPECT_EQ(c, -1);
+
+		a = 2;
+		b = -1;
+		c = a + b;
+
+		EXPECT_EQ(c, 1);
+
+		a = 536870911;
+		b = -536870911;
+		c = a + b;
+
+		EXPECT_EQ(c, 0);
+
+		a = 11111;
+		b = -1111111112;
+		c = a + b;
+
+		EXPECT_EQ(c, -1111100001);
+		
+		a = -11111;
+		b = 1111111112;
+		c = a + b;
+		
+		EXPECT_EQ(c, 1111100001);
+
+		// Also test a few with reversed lhs and rhs
+
+		a = 536870911;
+		b = -536870911;
+		c = b + a;
+		
+		EXPECT_EQ(c, 0);
+
+		a = 11111;
+		b = -1111111112;
+		c = b + a;
+
+		EXPECT_EQ(c, -1111100001);
+	}
+	
+	// Test addition of small integers
+	{
+		su::big_int_t a = 123456789;
+		su::big_int_t b = -987654321;
+		su::big_int_t c = a + b;
+		
+		EXPECT_EQ(c, -864197532);
+		
+		a = -2147483647;
+		b = 1;
+		c = a + b;
+		
+		EXPECT_EQ(c, -2147483646);
+		
+		a = 2147483647;
+		b = -2;
+		c = a + b;
+		
+		EXPECT_EQ(c, 2147483645);
+		
+		a = -2147483647;
+		b = 2147483647;
+		c = a + b;
+		
+		EXPECT_EQ(c, 0);
+		
+		a = -576460752303423480LL;
+		b = 576460752303423480LL;
+		c = a + b;
+		
+		EXPECT_EQ(c, 0);
+		
+		a = 132922799578491587LL;
+		b = -132922799578491529LL;
+		c = a + b;
+		
+		EXPECT_EQ(c, 58);
+	}
+	
+	// Test addition of large integers (using string initializer)
+	{
+
+		su::big_int_t a = su::big_int_t("123456789123456789123456789123456789");
+		su::big_int_t b = su::big_int_t("-987654321987654321987654321987654321");
+		su::big_int_t c = a + b;
+
+		EXPECT_EQ(c, su::big_int_t("-864197532864197532864197532864197532"));
+
+		a = su::big_int_t("18446744073709551615");
+		b = su::big_int_t("-18446744073709551615");
+		c = a + b;
+
+		EXPECT_EQ(c, su::big_int_t("0"));
+
+		a = su::big_int_t("-18446744073709551615");
+		b = su::big_int_t("5215321523512");
+		c = a + b;
+
+		EXPECT_EQ(c, su::big_int_t("-18446738858388028103"));
+
+		a = su::big_int_t("999999999999999999999999");
+		b = su::big_int_t("-1");
+		c = a + b;
+
+		EXPECT_EQ(c, su::big_int_t("999999999999999999999998"));
+
+		a = su::big_int_t("-999999999999999999999999");
+		b = su::big_int_t("999999999999999999999991");
+		c = a + b;
+
+		EXPECT_EQ(c, su::big_int_t("-8"));
+
+		a = su::big_int_t("1329227995784915872903807060278812422");
+		b = su::big_int_t("-1329227995784915872903690623925709942");
+		c = a + b;
+
+		EXPECT_EQ(c, su::big_int_t("148429635350912"));
+	}
+}
---
+++ tests/int_tests/string.cpp
@@ -0,0 +1,327 @@
+#include <gtest/gtest.h>
+
+#include <big_int.hpp>
+
+#include <sstream>
+#include <string>
+
+
+namespace su = suuri;
+
+
+TEST(IntString, DirectBase10Constructor)
+{
+	// Test that small numbers initialised with a string are equal to their builtin primitive counterpart
+	su::big_int_t a = 0;
+	su::big_int_t b{"0"};
+	
+	ASSERT_EQ(a, b);
+	ASSERT_EQ(b, a);
+	
+	a = 1;
+	b = su::big_int_t("1");
+	
+	ASSERT_EQ(a, b);
+	ASSERT_EQ(b, a);
+	
+	a = -1;
+	b = su::big_int_t("-1");
+	
+	ASSERT_EQ(a, b);
+	ASSERT_EQ(b, a);
+	
+	a = 123456789;
+	b = su::big_int_t("123456789");
+	
+	ASSERT_EQ(a, b);
+	ASSERT_EQ(b, a);
+	
+	a = -123456789;
+	b = su::big_int_t("-123456789");
+	
+	ASSERT_EQ(a, b);
+	ASSERT_EQ(b, a);
+	
+	// Test number bigger than 2^31 and less than 2^32 - 1
+	a = 2147483647;
+	b = su::big_int_t("2147483647");
+	
+	ASSERT_EQ(a, b);
+	ASSERT_EQ(b, a);
+	
+	a = -2147483647;
+	b = su::big_int_t("-2147483647");
+	
+	ASSERT_EQ(a, b);
+	ASSERT_EQ(b, a);
+	
+	// Also test a 64 unsigned integer
+	a = 123456789123456789ULL;
+	b = su::big_int_t("123456789123456789");
+	
+	ASSERT_EQ(a, b);
+	ASSERT_EQ(b, a);
+	
+	a = 18446744073709551615ULL;
+	b = su::big_int_t("18446744073709551615");
+	
+	ASSERT_EQ(a, b);
+	ASSERT_EQ(b, a);
+}
+
+TEST(IntString, SpecifiedBaseConstructor)
+{
+	{
+		su::big_int_t a{"0"};
+		su::big_int_t b;
+		
+		for (size_t i = 2; i < 37; i++)
+		{
+			std::stringstream ss;
+			ss << 'b' << i << '_' << '0';
+			b = su::big_int_t(ss.str());
+			
+			EXPECT_EQ(a, b) << "Tested base is: " << i;
+			EXPECT_EQ(b, a) << "Tested base is: " << i;;
+		}
+		
+		a = su::big_int_t("1");
+		
+		for (size_t i = 2; i < 37; i++)
+		{
+			std::stringstream ss;
+			ss << 'b' << i << '_' << '1';
+			b = su::big_int_t(ss.str());
+			
+			EXPECT_EQ(a, b) << "Tested base is: " << i;
+			EXPECT_EQ(b, a) << "Tested base is: " << i;;
+		}
+		
+		// Test negative values
+		a = su::big_int_t("-1");
+		
+		for (size_t i = 2; i < 37; i++)
+		{
+			std::stringstream ss;
+			ss << 'b' << i << '_' << "-1";
+			b = su::big_int_t(ss.str());
+			
+			EXPECT_EQ(a, b) << "Tested base is: " << i;
+			EXPECT_EQ(b, a) << "Tested base is: " << i;;
+		}
+		
+		// We're going to do the rest of the tests using the number 123456789012345678909876543211234567890
+		
+		////// Binary (base 2)
+		a = su::big_int_t("123456789012345678909876543211234567890");
+		b = su::big_int_t("b2_1011100111000001110100110100101011000000001010111111110110001100010001011001110000110011110110100100010101111101010011011010010");
+		
+		EXPECT_EQ(a, b);
+		EXPECT_EQ(b, a);
+		
+		/////// Ternary (base 3)
+		b = su::big_int_t("b3_21111222001101112001112201120001100200212102112201220011122121021000210122012200");
+		
+		EXPECT_EQ(a, b);
+		EXPECT_EQ(b, a);
+		
+		/////// Quaternary (base 4)
+		b = su::big_int_t("b4_1130320032212211120001113332301202023032012132310202233222123102");
+		
+		EXPECT_EQ(a, b);
+		EXPECT_EQ(b, a);
+		
+		/////// Quinary (base 5)
+		b = su::big_int_t("b5_2102444444300043220033312343422310201232433102022133030");
+		
+		EXPECT_EQ(a, b);
+		EXPECT_EQ(b, a);
+		
+		/////// Senary or Seximal (base 6)
+		b = su::big_int_t("b6_5255412204241411042310553444014330331541545304030");
+		
+		EXPECT_EQ(a, b);
+		EXPECT_EQ(b, a);
+		
+		/////// Septenary or Septimal (base 7)
+		b = su::big_int_t("b7_1103504602560214512564065241004163456611625454");
+		
+		EXPECT_EQ(a, b);
+		EXPECT_EQ(b, a);
+		
+		/////// Octal (base 8)
+		b = su::big_int_t("b8_1347016464530012776614213160636644257523322");
+		
+		EXPECT_EQ(a, b);
+		EXPECT_EQ(b, a);
+		
+		/////// Nonary or Nonal (base 9)
+		b = su::big_int_t("b9_7458041461481501320772481804577230718180");
+		
+		EXPECT_EQ(a, b);
+		EXPECT_EQ(b, a);
+		
+		/////// Decimal (base 10)
+		// Yes this makes sense. We also have to make sure that explicitly declaring base 10 works.
+		b = su::big_int_t("b10_123456789012345678909876543211234567890");
+		
+		EXPECT_EQ(a, b);
+		EXPECT_EQ(b, a);
+		
+		/////// Undecimal, Unodecimal, or Undenary (base 11)
+		b = su::big_int_t("b11_3aa2716a7880a695301782402135a7305476a");
+		
+		EXPECT_EQ(a, b);
+		EXPECT_EQ(b, a);
+		
+		/////// Duodecimal or Dozenal (base 12)
+		b = su::big_int_t("b12_210b88ab57786788b71608520b9892195016");
+		
+		EXPECT_EQ(a, b);
+		EXPECT_EQ(b, a);
+		
+		/////// Tredecimal or Tridecimal (base 13)
+		b = su::big_int_t("b13_185a8cb4592941c511a06c6b3c548963106");
+		
+		EXPECT_EQ(a, b);
+		EXPECT_EQ(b, a);
+		
+		/////// Quattuordecimal or Quadrodecimal (base 14)
+		b = su::big_int_t("b14_1c0530ad373bc16512c9b9439c001d42d4");
+		
+		EXPECT_EQ(a, b);
+		EXPECT_EQ(b, a);
+		
+		/////// Quindecimal or Pentadecimal (base 15)
+		b = su::big_int_t("b15_2cdc8b3b93b1c8100a142eb6abcc67d60");
+		
+		EXPECT_EQ(a, b);
+		EXPECT_EQ(b, a);
+		
+		/////// Hexadecimal (base 16)
+		b = su::big_int_t("b16_5CE0E9A56015FEC622CE19ED22BEA6D2");
+		
+		EXPECT_EQ(a, b);
+		EXPECT_EQ(b, a);
+		
+		/////// Septendecimal or Heptadecimal (base 17)
+		b = su::big_int_t("b17_f129b7a38997dd0a868b5ac4d9d3f95");
+		
+		EXPECT_EQ(a, b);
+		EXPECT_EQ(b, a);
+		
+		/////// Octodecimal (base 18)
+		b = su::big_int_t("b18_2cee3e14d46232cc6eaa1214h0922d0");
+		
+		EXPECT_EQ(a, b);
+		EXPECT_EQ(b, a);
+		
+		/////// Undevicesimal or Nonadecimal (base 19)
+		b = su::big_int_t("b19_a374b6a63db2hc83d8219af8f6f95a");
+		
+		EXPECT_EQ(a, b);
+		EXPECT_EQ(b, a);
+		
+		/////// Vigesimal (base 20)
+		b = su::big_int_t("b20_25jg9i11dabhc25cf6bb393fag0jea");
+		
+		EXPECT_EQ(a, b);
+		EXPECT_EQ(b, a);
+		
+		/////// base 21
+		b = su::big_int_t("b21_bf7hf2ba267f87bb96f5j2cgdcdfi");
+		
+		EXPECT_EQ(a, b);
+		EXPECT_EQ(b, a);
+		
+		/////// base 22
+		b = su::big_int_t("b22_343ca1aib9632dljhf95h8i21ffea");
+		
+		EXPECT_EQ(a, b);
+		EXPECT_EQ(b, a);
+		
+		/////// base 23
+		b = su::big_int_t("b23_l2lj7lg2dhl0hcdjjb60beadfl6f");
+		
+		EXPECT_EQ(a, b);
+		EXPECT_EQ(b, a);
+		
+		/////// Quadravigesimal (base 24)
+		b = su::big_int_t("b24_6ggkffn6jankfe1ma2hcfah7830i");
+		
+		EXPECT_EQ(a, b);
+		EXPECT_EQ(b, a);
+		
+		/////// base 25
+		b = su::big_int_t("b25_25eoon04ha3i7jjcg21dei5ac8ff");
+		
+		EXPECT_EQ(a, b);
+		EXPECT_EQ(b, a);
+		
+		/////// Hexavigesimal (base 26)
+		b = su::big_int_t("b26_k1aiiie118m76cn1khkj60i6n06");
+		
+		EXPECT_EQ(a, b);
+		EXPECT_EQ(b, a);
+		
+		/////// Septemvigesimal (base 27)
+		b = su::big_int_t("b27_7dq1ae1ejf19inbejo4hg70lh5i");
+		
+		EXPECT_EQ(a, b);
+		EXPECT_EQ(b, a);
+		
+		/////// base 28
+		b = su::big_int_t("b28_2plb7o15rb1f5kp0jli44afjlki");
+		
+		EXPECT_EQ(a, b);
+		EXPECT_EQ(b, a);
+		
+		/////// base 29
+		b = su::big_int_t("b29_1505jcj8fef7q9fqs0n68bn52os");
+		
+		EXPECT_EQ(a, b);
+		EXPECT_EQ(b, a);
+		
+		/////// Trigesimal (base 30)
+		b = su::big_int_t("b30_eh3lnmsofja5d8htcmlg444m30");
+		
+		EXPECT_EQ(a, b);
+		EXPECT_EQ(b, a);
+		
+		/////// base 31
+		b = su::big_int_t("b31_6cumn16bf01d7d7i7a5otkriob");
+		
+		EXPECT_EQ(a, b);
+		EXPECT_EQ(b, a);
+		
+		/////// Duotrigesimal (base 32)
+		b = su::big_int_t("b32_2ss3kqao0lvr325jgptkhbt9mi");
+		
+		EXPECT_EQ(a, b);
+		EXPECT_EQ(b, a);
+		
+		/////// base 33
+		b = su::big_int_t("b33_1bch1qg2i8a9m7umg4q27b26vl");
+		
+		EXPECT_EQ(a, b);
+		EXPECT_EQ(b, a);
+		
+		/////// base 34
+		b = su::big_int_t("b34_ln1tiwhrb95n0t006c46n4p4m");
+		
+		EXPECT_EQ(a, b);
+		EXPECT_EQ(b, a);
+		
+		/////// base 35
+		b = su::big_int_t("b35_asdxq9h1ety28y9rrtliuvnyp");
+		
+		EXPECT_EQ(a, b);
+		EXPECT_EQ(b, a);
+		
+		/////// Hexatrigesimal (base 36)
+		b = su::big_int_t("b36_5hy8cqpp6qj5xsoal3jybtioi");
+		
+		EXPECT_EQ(a, b);
+		EXPECT_EQ(b, a);
+	}
+}
